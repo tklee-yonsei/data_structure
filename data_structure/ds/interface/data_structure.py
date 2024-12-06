3934fd8da9f176768c2bc36b9031d297f9d7d6d2
tklee-yonsei <taekyulee@yonsei.ac.kr>
2024-12-06 14:10:11 +0900

    * Add searchable

--- data_structure/ds/interface/data_structure.py
+++ data_structure/ds/interface/data_structure.py
@@ -2,9 +2,10 @@ from abc import ABC
 
 from data_structure.ds.interface.addable import Addable
 from data_structure.ds.interface.removable import Removable
+from data_structure.ds.interface.searchable import Searchable
 
 
-class DataStructure(Addable, Removable, ABC):
+class DataStructure(Addable, Removable, Searchable, ABC):
     """데이터 구조의 인터페이스."""
 
     pass
