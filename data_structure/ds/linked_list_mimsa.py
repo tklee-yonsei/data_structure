f3614e01485900869b883a9dbd9b33ac12157ae1
BOOK-95D1EP5V1T\고민서 <gom8gom@naver.com>
2024-12-06 14:43:14 +0900

    * mimsa frame add

---
+++ data_structure/ds/linked_list_mimsa.py
@@ -0,0 +1,14 @@
+from data_structure.ds.interface.data_structure import DataStructure
+
+
+class LinkedList(DataStructure):
+    """LinkedList 클래스."""
+    
+    def add(self, value):
+        pass
+
+    def remove(self, value):
+        return False
+
+    def search(self, value):
+        return False
