89d5e1ed39cb353ef9995b3e66f32ec951022028
tklee-yonsei <taekyulee@yonsei.ac.kr>
2024-12-11 13:10:09 +0900

    * Fix test

--- tests/ds/interface/base_test_data_structure.py
+++ tests/ds/interface/base_test_data_structure.py
@@ -4,7 +4,7 @@ from abc import ABC, abstractmethod
 from data_structure.ds.interface.data_structure import DataStructure
 
 
-class BaseDataStructureTest(ABC, unittest.TestCase):
+class _BaseDataStructureTest(ABC):
     """모든 데이터 구조 테스트 클래스가 상속할 기본 클래스"""
 
     @abstractmethod
